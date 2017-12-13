600f844489ef9cbb8a941d0c2f60a07e131c18a6
borrascador <roflbot101@gmail.com>
2017-12-12 20:55:51 -0800

    First working Webpack build

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: "./index",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif|woff|woff2|eot|ttf)$/,
+        use: [
+          'url-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+}
