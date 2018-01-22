b2b14fe797ef9e46b03ff47b5d4eac9e250ab073
borrascador <roflbot101@gmail.com>
2018-01-21 20:58:41 -0500

    Brainstorming object stuff

--- src/engine/sharedobjects.js
+++ src/engine/sharedobjects.js
@@ -1,20 +1,46 @@
+const [WOOD, ORE, OIL] = [‘WOOD’, ‘ORE’, ‘OIL’];
+const [FUEL, AMMO, FOOD] = [‘FUEL’, ‘AMMO’, ‘FOOD’];
+const [TIRE, PARTS, BATTERY] = [‘TIRE’, ‘PARTS’, ‘BATTERY’];
+const [MULE, TRUCK, CANOE, BOAT] = [‘MULE’, ‘TRUCK’, ‘CANOE’, ‘BOAT’];
 
-const rawResources = ['WOOD', 'STONE', 'ORE', 'OIL', '', 'FISH'];
-const processedResources = ['FUEL', 'TRUCKS', 'CANOES', 'AMMO', 'CARPARTS', 'TOOLS'];
-const tileTypes = ['RIVER', 'ROAD', 'FOREST', 'PLAIN'];	
 
-Const item () => {
-	Return {
-		Type: ITEM
-		ResourceType: string
-		RepairStatus: string
-		Quantity: integer
+const item () => {
+	return {
+		type: ITEM
+		resourceType: string
+		repairStatus: string
+    stackable: bool
+		quantity: integer
 	}
 }
 
-Const assignResourceType(item, resourceType) => {
-	Return Object.assign({}, item, {
+const setResourceType(item, resourceType) => {
+	return Object.assign({}, item, {
 		resourceType: resourceType
 	});
+}
+
+function itemReducer(state, action) {
+  switch (action.type) {
+    case ADD_ITEM:
+      if (state.items.filter(item => 
+            item.resourceType === action.resourceType
+            && item.stackable === true)) {
+
+      // if state.items already includes an ITEM with the same resourceType as the new item and the item is stackable, then add the 
+      return Object.assign({}, state, {
+        items: [
+          ...state.items,
+          {
+            type: ITEM,
+            resourceType: action.resourceType,
+            repairStatus: action.repairStatus,
+            quantity: action.quantity
+          }
+        ]
+      })
+    default:
+      return state
+  }
 }
 
