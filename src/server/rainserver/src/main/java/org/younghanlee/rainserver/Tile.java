6707f5deae1e6a7b29e7e1c633384bcd24a02b70
amos1814 <amos1814@gmail.com>
2018-06-02 18:22:55 -0400

    load map from tiles.json

--- src/server/rainserver/src/main/java/org/younghanlee/rainserver/Tile.java
+++ src/server/rainserver/src/main/java/org/younghanlee/rainserver/Tile.java
@@ -1,4 +1,7 @@
 package org.younghanlee.rainserver;
+
+import org.json.JSONObject;
+
 public class Tile {
 	// Position
 	private int id;
@@ -13,11 +16,23 @@ public class Tile {
 	private Player owner;
 	private Player[] visitors;
 	
-	public Tile(int id, int mapWidth) {
-		this.id = id;
-		this.x = id % mapWidth;
-		this.y = (id - x)/mapWidth;
+	public Tile(JSONObject jo) {
+		this.id = jo.getInt("id");
+		this.x = jo.getInt("x");
+		this.y = jo.getInt("y");
 		this.visitors = new Player[0];
+		
+		JSONObject layers = jo.getJSONObject("layers");
+		if (layers.has("base")) {
+			this.base = layers.getInt("base");
+		}
+		if (layers.has("middle")) {
+			this.middle = layers.getInt("middle");
+		}
+		if (layers.has("top")) {
+			this.top = layers.getInt("top");
+		}
+		
 	}
 	
 	public void claim(Player p) {
