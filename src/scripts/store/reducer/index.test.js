c1b16fdc5bc96904e8faf58b71e4b16191775044
Jan Tabaczynski <jantabaczynski@gmail.com>
2018-10-01 00:47:10 -0700

    Added jest for testing

---
+++ src/scripts/store/reducer/index.test.js
@@ -0,0 +1,51 @@
+import reducer from './index.js';
+import { initialState } from './initialState';
+import * as actions from '../actions/actions';
+
+describe('reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should handle ADD_TODO', () => {
+    expect(
+      reducer([], {
+        type: types.ADD_TODO,
+        text: 'Run the tests'
+      })
+    ).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 0
+      }
+    ])
+
+    expect(
+      reducer(
+        [
+          {
+            text: 'Use Redux',
+            completed: false,
+            id: 0
+          }
+        ],
+        {
+          type: types.ADD_TODO,
+          text: 'Run the tests'
+        }
+      )
+    ).toEqual([
+      {
+        text: 'Run the tests',
+        completed: false,
+        id: 1
+      },
+      {
+        text: 'Use Redux',
+        completed: false,
+        id: 0
+      }
+    ])
+  })
+})
