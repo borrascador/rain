5c54c37ce355c7cf74055baeacfc6d2fd260b238
amos1814 <amos1814@gmail.com>
2018-10-05 00:13:43 -0400

    adding more multipliers

--- src/server/rainserver/src/main/java/org/younghanlee/rainserver/IChoice.java
+++ src/server/rainserver/src/main/java/org/younghanlee/rainserver/IChoice.java
@@ -6,9 +6,6 @@ import org.json.JSONObject;
 
 public interface IChoice{
 	String getText(Player p);
-	JSONObject result(Player p);
-	ArrayList<Multiplier> multipliers = new ArrayList<Multiplier>();
-	public static void addMultiplier(Multiplier m) {
-		multipliers.add(m);
-	}
+	JSONObject result(Player p, ArrayList<Multiplier> multipliers);
+	ArrayList<Multiplier> generateMultipliers(Player p);
 }
