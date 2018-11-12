d024625f269056e5d01e3e2ba8ec48a7131a0249
Jan Tabaczynski <roflbot101@gmail.com>
2018-11-11 18:47:02 -0800

    dev-197 Add tests for zoom, make new initial state function for testing

--- src/scripts/store/reducer/index.test.js
+++ src/scripts/store/reducer/index.test.js
@@ -1,23 +1,55 @@
 import reducer from './index.js';
-import { initialState } from './initialState';
+import { newInitialState } from './initialState';
 import * as actions from '../actions/actions';
 import { ACTIONS, EVENTS } from '../actions/types';
 
 describe('reducer', () => {
   it('should return the initial state', () => {
+    let initialState = newInitialState()
     expect(reducer(undefined, {})).toEqual(initialState)
   })
 
-  it('should ZOOM_IN correctly', () => {
+  it('should zoom in and out correctly', () => {
+    let initialState = newInitialState();
     expect(
       reducer(initialState, {
-        type: actions.ZOOM_IN
+        type: ACTIONS.ZOOM_IN
+      })
+    ).toEqual(
+      Object.assign(
+        initialState,
+        { zoom: 4 }
+      )
+    )
+
+    initialState = newInitialState({ zoom: 5 });
+    expect(
+      reducer(initialState, {
+        type: ACTIONS.ZOOM_IN
+      })
+    ).toEqual(
+      initialState
+    )
+
+    initialState = newInitialState();
+    expect(
+      reducer(initialState, {
+        type: ACTIONS.ZOOM_OUT
       })
     ).toEqual(
       Object.assign(
         initialState,
         { zoom: 2 }
       )
+    )
+
+    initialState = newInitialState({ zoom: 1 });
+    expect(
+      reducer(initialState, {
+        type: ACTIONS.ZOOM_OUT
+      })
+    ).toEqual(
+      initialState
     )
   })
 })
